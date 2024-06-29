52966f7540da59ac93ab524cb3dc35a5bbdb3ce8
danilojezernik <dani.jezernik@gmail.com>
2024-06-29 18:15:30 +0200

    1. Remove svg for current weather

--- src/app/core/data-get-time/data-get-time.component.ts
+++ src/app/core/data-get-time/data-get-time.component.ts
@@ -3,19 +3,6 @@ import { CommonModule } from '@angular/common';
 import { WeatherService } from "../../services/api/weather.service";
 import { Observable } from "rxjs";
 import { WeatherCurrent } from "../../models/weather";
-import { SunSvgComponent } from "../../shared/components/weather-svg/sun-svg/sun-svg.component";
-import { FewCloudsSvgComponent } from "../../shared/components/weather-svg/few-clouds-svg/few-clouds-svg.component";
-import {
-  ThunderstormSvgComponent
-} from "../../shared/components/weather-svg/thunderstorm-svg/thunderstorm-svg.component";
-import { RainSvgComponent } from "../../shared/components/weather-svg/rain-svg/rain-svg.component";
-import {
-  ScatteredCloudsSvgComponent
-} from "../../shared/components/weather-svg/scattered-clouds-svg/scattered-clouds-svg.component";
-import { SnowSvgComponent } from "../../shared/components/weather-svg/snow-svg/snow-svg.component";
-import {
-  BrokenCloudsSvgComponent
-} from "../../shared/components/weather-svg/broken-clouds-svg/broken-clouds-svg.component";
 
 /**
  * Component displaying city name and the date and time of the last update.
@@ -23,7 +10,7 @@ import {
 @Component({
   selector: 'app-data-get-time',
   standalone: true,
-  imports: [ CommonModule, SunSvgComponent, FewCloudsSvgComponent, ThunderstormSvgComponent, RainSvgComponent, ScatteredCloudsSvgComponent, SnowSvgComponent, BrokenCloudsSvgComponent ],
+  imports: [ CommonModule ],
   templateUrl: './data-get-time.component.html'
 })
 export class DataGetTimeComponent {
