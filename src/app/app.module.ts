0310d67e61d7d065446f449e79e5e569d3cf469f
danilojezernik <dani.jezernik@gmail.com>
2024-06-29 18:20:24 +0200

    1. Install ngx-translator and set up translate module and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
 import { DataGetTimeComponent } from "./core/data-get-time/data-get-time.component";
 import { DataGetTempComponent } from "./core/data-get-temp/data-get-temp.component";
 import { DataGetForecastComponent } from "./core/data-get-forecast/data-get-forecast.component";
@@ -10,6 +10,10 @@ import { HeaderComponent } from "./core/header/header.component";
 import { SpinnerComponent } from "./shared/components/spinner/spinner.component";
 import { RefreshButtonComponent } from "./shared/components/refresh-button/refresh-button.component";
 import { DateTransformPipe } from './pipes/date-transform.pipe';
+
+// import ngx-translate and the http loader
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
@@ -24,10 +28,23 @@ import { DateTransformPipe } from './pipes/date-transform.pipe';
     HeaderComponent,
     SpinnerComponent,
     RefreshButtonComponent,
-    DateTransformPipe
+    DateTransformPipe,
+
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [ HttpClient ]
+      }
+    })
   ],
   providers: [],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {
+}
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
 }
