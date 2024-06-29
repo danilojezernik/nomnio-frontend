89c0820b060450a9a2e1597f03a62d0b592a926e
danilojezernik <dani.jezernik@gmail.com>
2024-06-29 18:09:14 +0200

    1. Adding  new style for app: container, content, info, forecast,... 2. adding new comments.

--- src/app/shared/components/weather-svg/sun-svg/sun-svg.component.ts
+++ src/app/shared/components/weather-svg/sun-svg/sun-svg.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sun-svg',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './sun-svg.component.html'
 })
 export class SunSvgComponent {
