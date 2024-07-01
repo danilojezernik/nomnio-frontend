bf4b2b3265af67fda5004e0aad8ddf0b54e17520
danilojezernik <dani.jezernik@gmail.com>
2024-07-01 06:40:11 +0200

    1. Adding new style for app. 2. Adding 100kb to production budget to clear warning. 3. Updated README file. 4. Added new refresh button.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production: false,
   urlLocalForecast: 'http://localhost:8080/weather-forecast',
   urlLocalCurrent: 'http://localhost:8080/weather-current',
 };
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production: true,
   urlLocalForecast: 'https://nomnio-api-0cacfbde4021.herokuapp.com/weather-forecast',
   urlLocalCurrent: 'https://nomnio-api-0cacfbde4021.herokuapp.com/weather-current',
 };
